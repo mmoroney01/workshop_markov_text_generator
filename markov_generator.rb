3b916224300dba5fdfff29226c26962d2cd83f64
Michael Moroney <mmoroney06@gmail.com>
2018-01-10 19:22:12 -0600

    chain now uses two words to decide next word

--- markov_generator.rb
+++ markov_generator.rb
@@ -29,4 +29,41 @@ class MarkovGenerator
 
     sentence.join(" ")
   end
+
+  def self.from_file_two file
+    from_text_two File.read(file)
+  end
+
+  def self.from_text_two text
+    freq_hash = Hash.new {|hash, key| hash[key] = [] }
+
+    #put spaces before ending punctuation to count them as words.
+    text.gsub!(".", "\s.")
+    text.gsub!("!", "\s!")
+    text.gsub!("?", "\s?")
+
+    text.split.each_cons(3) do |word1, word2, following_word|
+      arr = [word1, word2]
+      freq_hash[arr] << following_word
+    end
+
+
+    require 'pp'
+    pp freq_hash
+    current_words = freq_hash.keys.sample
+    p current_words
+    sentence = current_words[0] + " " + current_words[1]
+
+    12.times do
+      next_word_first = current_words[1]
+
+      next_word = freq_hash[current_words].sample
+
+      sentence << " " + next_word
+
+      current_words = [current_words[1], next_word]
+    end
+
+    sentence
+  end
 end
